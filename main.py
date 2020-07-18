8edf4de56f08c7fe3f2903e4df656932407bf78c
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-07-18 17:32:46 +0900

    response image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -8,8 +9,9 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
+    description: Optional[str] = None
     price: float
-    is_offer: Optional[bool] = None
+    tax: Optional[float]
 
 
 @app.get("/")
@@ -17,11 +19,27 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get(
+        "/items/{item_id}",
+        response_model=Item,
+        responses={
+            200: {
+                "content": {"image/png": {}},
+                "description": "Return the JSON item or an image."
+                }
+            },
+        )
+def read_item(item_id: int, img: Optional[bool] = None, q: Optional[str] = None):
+    if img:
+        return FileResponse("image.png", media_type="image/png")
+    else:
+        return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
